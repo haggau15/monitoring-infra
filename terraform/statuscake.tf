2f39ad8f3e2970ef615ba0580ef1c798387ed0eb
Gaute Hagen <lybakk@gmail.com>
2018-11-05 10:44:02 +0100

    lol

--- terraform/statuscake.tf
+++ terraform/statuscake.tf
@@ -1,5 +1,5 @@
 provider "statuscake" {
-  username = "glennbechgmailcom"
+  username = "haggau94gmailcom"
 }
 
 resource "statuscake_test" "production" {
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,7 +1,7 @@
 # Modify this file to make it your own :-)
 
 variable "app_prefix" {
-  default = "gbmon"
+  default = "ghgen"
 }
 
 variable "pipeline_name" {
